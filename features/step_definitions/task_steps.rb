cdb8d15b97b86e69283ade2ce374ebdc87884721
Wes Simpson <wss6040@nyu.edu>
2025-11-06 14:33:58 -0500

    RED: Add failing step definitions and data models for create task feature

---
+++ features/step_definitions/task_steps.rb
@@ -0,0 +1,38 @@
+Given("a project named {string} exists") do |project_name|
+  Project.create!(name: project_name)
+end
+
+Given("I am on the {string} project's tasks page") do |project_name|
+  project = Project.find_by!(name: project_name)
+  visit project_tasks_path(project)
+end
+
+When("I click {string}") do |button_text|
+  begin
+    click_button button_text
+  rescue Capybara::ElementNotFound
+    click_link button_text
+  end
+end
+
+When("I fill in {string} with {string}") do |field_name, value|
+  fill_in field_name, with: value
+end
+
+When("I select {string} from {string}") do |option, field_name|
+  select option, from: field_name
+end
+
+Then("I should see {string}") do |text|
+  expect(page).to have_content(text)
+end
+
+Then("I should see the task in the list for project {string}") do |project_name|
+  project = Project.find_by!(name: project_name)
+  expect(page).to have_current_path(project_tasks_path(project))
+end
+
+When("I leave {string} blank") do |field_name|
+  fill_in field_name, with: ""
+end
+
