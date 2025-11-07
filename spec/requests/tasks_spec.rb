5a1f9592c923b8b0ef069a4589c2cc9832e70cba
Perry Huang <perryhuang2015@gmail.com>
2025-11-06 20:29:59 -0500

    test: add tests for get and posting tasks

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
-  describe "GET /create" do
+  describe "GET /new" do
     it "returns http success" do
-      get "/tasks/create"
+      get "/tasks/new"
       expect(response).to have_http_status(:success)
     end
   end
-
+  describe "POST /" do
+    it "returns http success" do
+      post "/tasks"
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
