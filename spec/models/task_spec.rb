0df37f33700c8cc1c0e08905a0341dadc08895bb
Zesan Rahman <ramenzzz64@gmail.com>
2025-11-06 20:42:47 -0500

    RED: Finalized correct failing rspec tests

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe Task, type: :model do
 
   context "creating a task in project Alpha" do
     it "creates a task with valid inputs" do
-      @task = Task.create!(title: "Implement WIP limit", status: "To Do")
-      expect(task).to_be valid
+      @task = Task.create!(title: "Implement WIP limit", status: "To Do", project_id: @project[:id])
+      expect(@task).to be_valid
     end
 
     it "fails to create task with title" do
-      @task = Task.create!(title: "", status: "To Do")
-      expect(task).to_be not_valid
+      @task = Task.new(title: "", status: "To Do", project_id: @project[:id])
+      expect(@task).to_not be_valid
     end
   end
 end
