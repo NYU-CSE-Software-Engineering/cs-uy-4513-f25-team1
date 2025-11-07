f24a6a4a381fc51f5add4ee0eea2eb51556bfbbe
Perry Huang <perryhuang2015@gmail.com>
2025-11-06 21:42:31 -0500

    feat: change paths of test and add test project

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
-  describe "GET /new" do
+  before(:context) do
+    if Project.find_by(1)
+      return
+    end
+
+    Project.create(name: "Test project")
+  end
+
+  describe "GET /projects/id/tasks/new" do
     it "returns http success" do
-      get "/tasks/new"
+      get "/projects/1/tasks/new"
       expect(response).to have_http_status(:success)
     end
   end
-  describe "POST /" do
+  describe "POST /projects/id/tasks" do
     it "returns http success" do
-      post "/tasks"
+      post "/projects/1/tasks"
       expect(response).to have_http_status(:success)
     end
   end
