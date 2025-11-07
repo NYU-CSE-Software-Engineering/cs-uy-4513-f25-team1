f255a158304dab53949a45775c977c37ea59d034
Zesan Rahman <ramenzzz64@gmail.com>
2025-11-06 20:25:26 -0500

    Red: Changed task_spec to better match scenario

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,25 +3,12 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do 
   before do
     @project = Project.create!(name: "Alpha")
-
-    visit project_tasks_path(@project)
   end
 
-  context "when creating a task with valid inputs" do
-    it "creates the task successfully" do
-      click_link "New Task"
-
-      fill_in "Title", with: "Implement WIP limit"
-
-      select "To Do", from: "Status"
-
-      click_button "Create Task"
-
-      expect(page).to have_content("Implement WIP limit")
-
-      within(".task-list") do
-        expect(page).to have_content("Implement WIP limit")
-      end
+  context "creating a task in project Alpha" do
+    it "creates a task with valid inputs" do
+      @task = Task.create!(title: "Implement WIP limit", status: "To Do")
+      expect(task).to_be valid
     end
   end
 end
