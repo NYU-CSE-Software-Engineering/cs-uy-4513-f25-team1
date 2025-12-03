7680e17e8bcda6e711b79c2570ad7d53d86651c7
Zesan Rahman <ramenzzz64@gmail.com>
2025-12-02 22:52:12 -0500

    feat: make user model, need to change password to password_digest

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_secure_password #keyword that confirms password, requires password to be renamed to password_digest and requires bcrypt gem
+
+  validates :email, presence: true, uniqueness: true, format: {
+    with: /\A[^@\s]+@[^@\s]+\z/, #Regex for valid looking email
+    message: "must be a valid email address"
+  }
+
+  validates :username, presence: true, length: { minimum: 3, maximum: 20 }
+
+  validates :password, presence: true, length: { minimum: 6 }, confirmation: true
+  validates :password_confirmation, presence: true
+end
