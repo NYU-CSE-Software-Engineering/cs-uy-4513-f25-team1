20ae5b477c4aa6b15488f5f72d359ad113ed3e3b
Wes Simpson <wss6040@nyu.edu>
2025-12-03 11:21:20 -0500

    feat: Introduce FactoryBot factories for `User`, `Project`, `Task`, and `Collaborator` models, add `belongs_to :user` to `Task`, and update `db/seeds.rb` to generate sample data.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ApplicationRecord
   self.inheritance_column = :_type_disabled # Disable STI to allow 'type' as a regular column
   belongs_to :project
+  belongs_to :user
+
 
   validates :title, presence: true
   validates :status, presence: true
