3cdc087029fd629001b46aea11239a105e5fc365
Wes Simpson <wss6040@nyu.edu>
2025-12-09 17:14:23 -0500

    Remove WIP limit test files

--- spec/models/wip_limit_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe Task, type: :model do
-  it "has a limit of 2 tasks" do
-    project = Project.create!(name: "Alpha", wip_limit: 2)
-    expect(project.get_task_limit).to eq(2)
-    expect(project.count_tasks).to eq(0)
-  end
-end
