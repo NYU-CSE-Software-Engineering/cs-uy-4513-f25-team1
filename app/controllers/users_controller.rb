acbae6f764885867503c7ded2bea3829c0e2e56c
Perry Huang <perryhuang2015@gmail.com>
2025-12-10 09:56:44 -0500

    feat: add user#edit and user#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(session[:user_id])
+  end
+
+  def update
+    @user = User.find(session[:user_id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to "/"
+    else
+      render :edit
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
