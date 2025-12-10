1102dbd7e523b4b229d2da5dc17a7aef15d7bf13
Perry Huang <perryhuang2015@gmail.com>
2025-12-10 14:51:14 -0500

    feat(user): add rspec request test for edit

---
+++ spec/requests/edit_user_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "Edit user", type: :request do
+  let (:user) do
+    user = User.create(email_address: "edit@test.com", username: "edit", password: "test")
+    post "/session", params: { email_address: user.email_address, password: user.password }
+    user
+  end
+
+  it "renders the edit page" do
+    @user = user
+
+    get edit_user_path(@user)
+
+    expect(response).to have_http_status(:success)
+  end
+
+  it "redirects me to projects upon successful edit" do
+    @user = user
+
+    patch user_path(@user), params: { user: { email_address: "other@other.com", username: "other", password: "other", password_confirmation: "other" } }
+
+    expect(User.find(@user.id).email_address).to eq("other@other.com")
+    expect(response).to have_http_status(:see_other)
+  end
+
+  it "redirects me to the edit page upon unsuccessful edit" do
+    @user = user
+
+    patch user_path(@user), params: { user: { email_address: "notanemail", username: "other", password: "other", password_confirmation: "other" } }
+
+    expect(User.find(@user.id).email_address).not_to eq("notanemail")
+    expect(response).to have_http_status(:bad_request)
+  end
+end
