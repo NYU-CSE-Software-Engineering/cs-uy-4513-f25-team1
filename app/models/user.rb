ff7800dc8e1186d61cd6bab834be2fb47f747e20
Perry Huang <perryhuang2015@gmail.com>
2025-12-10 14:51:31 -0500

    feat(user): add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
   has_many :collaborators, dependent: :destroy
   has_many :projects, through: :collaborators
+  validates :email_address, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
